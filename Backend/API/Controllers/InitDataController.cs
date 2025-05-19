788234f0adf23aa4cdea46f6db28be03777316a1
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2025-05-19 15:33:00 +0200

    Add InitDataController for Wuwa data initialization
    Introduces `InitDataController` to handle HTTP GET requests for initializing Wuwa-related data. Implements caching to optimize performance by storing and retrieving data from a distributed cache. If cached data is available, it is returned; otherwise, data is fetched from the database and cached. Also adds `WuwaInitDto` to structure the response data, including lists of `WuwaMainStat` and `WuwaSubStat`.

---
+++ Backend/API/Controllers/InitDataController.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using API.Data;
+using API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InitDataController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IDistributedCache _cache;
+
+        public InitDataController(AppDbContext context, IDistributedCache cache)
+        {
+            _context = context;
+            _cache = cache;
+        }
+
+        [HttpGet("initWuwa")]
+        public async Task<IActionResult> GetWuwaInitData()
+        {
+            const string cacheKey = "WuwaInitData";
+
+            string? cachedData = await _cache.GetStringAsync(cacheKey);
+
+            // check if data is already cached, if so, return it
+            if (!string.IsNullOrEmpty(cachedData))
+            {
+                var deserializedData = JsonSerializer.Deserialize<List<WuwaInitDto>>(cachedData);
+                return Ok(deserializedData);
+            }
+
+            // if not cached, fetch data from the database
+            var result = new WuwaInitDto
+            {
+                MainStats = await _context.WuwaMainStats.ToListAsync(),
+                SubStats = await _context.WuwaSubStats.ToListAsync()
+            };
+
+            var serialized = JsonSerializer.Serialize(result);
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromDays(1) // Cache for 1 day
+            };
+
+            // Set the cache
+            await _cache.SetStringAsync(cacheKey, serialized, options);
+            return Ok(result);
+        }
+    }
+}
---
+++ Backend/API/Dtos/WuwaInitDto.cs
@@ -0,0 +1,10 @@
+﻿using API.Models;
+
+namespace API.Dtos
+{
+    public class WuwaInitDto
+    {
+        public List<WuwaMainStat> MainStats { get; set; } = new List<WuwaMainStat>();
+        public List<WuwaSubStat> SubStats { get; set; } = new List<WuwaSubStat>();
+    }
+}
