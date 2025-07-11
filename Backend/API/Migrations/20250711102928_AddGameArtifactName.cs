009574ad13d50b1458621a01a622c029fcfedee2
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2025-07-11 12:29:55 +0200

    Add GameArtifactName entity and migration
    Introduce a new `GameArtifactName` entity with a corresponding
    `GameArtifactNames` table in the database schema. This includes
    properties for `Id`, `Name`, and `GameId`, with `Id` set to
    auto-generate using PostgreSQL's identity column feature.
    Establish a foreign key relationship with the existing `Game`
    entity, enforcing referential integrity with cascade delete.
    Update the model snapshot to reflect these changes and
    optimize queries with an index on the `GameId` column.

---
+++ Backend/API/Migrations/20250711102928_AddGameArtifactName.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameArtifactName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameArtifactNames",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    GameId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameArtifactNames", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameArtifactNames_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameArtifactNames_GameId",
+                table: "GameArtifactNames",
+                column: "GameId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameArtifactNames");
+        }
+    }
+}
