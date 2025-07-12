8e017f11d56ab5c0c36d7f597f1846fad3347eaf
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2025-07-12 05:49:43 +0200

    Add GameArtifactNameRepository and interface methods
    Introduce `GameArtifactNameRepository` to handle data operations for `GameArtifactName` entities. Implement asynchronous methods to retrieve game artifact names by game ID and game name. Update `IGameArtifactNameRepository` to include these methods for consistency across implementations.

---
+++ Backend/API/Repositories/GameArtifactNameRepository.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories
+{
+    public class GameArtifactNameRepository : GenericRepository<GameArtifactName>, IGameArtifactNameRepository
+    {
+        public GameArtifactNameRepository(AppDbContext context) : base(context)
+        {
+
+        }
+
+        public async Task<IEnumerable<GameArtifactName>> GetGameArtifactNamesByGameIdAsync(int gameId)
+        {
+            return await _dbSet
+                .Where(g => g.Id == gameId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<GameArtifactName>> GetGameArtifactNamesByGameNameAsync(string gameName)
+        {
+            return await _dbSet
+                .Include(g => g.Game)
+                .Where(g => g.Game.Name == gameName)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Backend/API/Repositories/IGameArtifactNameRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Models;
+
+namespace API.Repositories
+{
+    public interface IGameArtifactNameRepository : IGenericRepository<GameArtifactName>
+    {
+        Task<IEnumerable<GameArtifactName>> GetGameArtifactNamesByGameNameAsync(string gameName);
+        Task<IEnumerable<GameArtifactName>> GetGameArtifactNamesByGameIdAsync(int gameId);
+    }
+}
